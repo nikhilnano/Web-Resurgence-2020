7da69a4bf923bd30c668d5a0fb0f61386fccd824
Vasu Dev Sall <nileshsingh217@gmail.com>
2020-03-02 20:01:24 +0530

    Loading after complete page ready

--- js/index.js
+++ js/index.js
@@ -92,40 +92,47 @@ window.addEventListener('mousewheel', function(e){
     }
 });
 
+function afterLoad()
+{
+  const a = document.getElementById('loader');
+  const c = document.getElementById('sliding-content-home');
+  const b = document.getElementById('LandingVideo');
+  const d = document.getElementById('main-content');
+  console.log(a, c);
+  a.classList.add("animated");
+  a.classList.add("fadeOut");
+  console.log("Entering the animation");
+  a.addEventListener('animationend', function() {
+    console.log("Animation ended");
+    a.classList.add("invisible");
+    d.classList.remove("invisible");
+    c.classList.remove("invisible");
+    c.classList.add("animated");
+    c.classList.add("fadeIn");
+    // c.classList.add("slower");
+    c.addEventListener('animationend', function()	{
+      c.classList.remove("fadeIn");
+      // c.classList.remove("slower");
+      b.autoplay = true;
+      b.muted = true;
+      b.load();
+    });
+    (function () {
+      setTimeout(function() {
+          console.log('End');
+          var e = document.getElementById('LoopVideo');
+          b.classList.add('invisible');
+          e.classList.remove('invisible');
+      }, 3800);
+    }());
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function(event){
   setTimeout(function() {
-      const a = document.getElementById('loader');
-      const c = document.getElementById('sliding-content-home');
-      const b = document.getElementById('LandingVideo');
-      const d = document.getElementById('main-content');
-      console.log(a, c);
-      a.classList.add("animated");
-      a.classList.add("fadeOut");
-      console.log("Entering the animation");
-      a.addEventListener('animationend', function() {
-        console.log("Animation ended");
-        a.classList.add("invisible");
-        d.classList.remove("invisible");
-        c.classList.remove("invisible");
-        c.classList.add("animated");
-        c.classList.add("fadeIn");
-        // c.classList.add("slower");
-        c.addEventListener('animationend', function()	{
-          c.classList.remove("fadeIn");
-          // c.classList.remove("slower");
-          b.autoplay = true;
-          b.muted = true;
-          b.load();
-        });
-        (function () {
-          setTimeout(function() {
-              console.log('End');
-              var d = document.getElementById('LoopVideo');
-              b.classList.add('invisible');
-              d.classList.remove('invisible');
-          }, 6000);
-        }());
-      });
+    console.log("Hi");
+    $(window).on("load", afterLoad());
+
   }, 4100);
 });
 
